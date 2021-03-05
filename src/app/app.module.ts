995a26fa5078e0de0b6dea1b68a30068f677ecee
kirilnikolov <kiril.nikolov.97@gmail.com>
2021-03-05 07:49:09 +0200

    Create front-end part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import {ChildrenOutletContexts, RouterModule} from '@angular/router';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {MatNativeDateModule} from '@angular/material/core';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ChildrenOutletContexts, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/student.model.ts
@@ -0,0 +1,18 @@
+
+export class Student {
+
+  id: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  dob: Date;
+
+  constructor() {
+    this.id = null;
+    this.firstName = null;
+    this.lastName = null;
+    this.gender = null;
+    this.dob = null;
+  }
+
+}
